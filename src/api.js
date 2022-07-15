e3cf1f683692ec2d0a7bdef1e81e67ad3c4928d2
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-15 18:00:09 -0300

    chore: Req 03 (Aplicação) - Cria rota 'login' com verbo http 'GET'

--- src/api.js
+++ src/api.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 // ...
 
+const UserController = require('./controllers/UserController');
+
 const app = express();
 
 app.use(express.json());
+
+app.get('/login', UserController.getLogin);
 
 // ...
 
