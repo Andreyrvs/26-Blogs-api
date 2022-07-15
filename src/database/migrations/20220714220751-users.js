62d019d90446b7d733cd19276f7d0d04f1afc086
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-15 16:37:48 -0300

    chore: Req 01 (Migration) - Cria migration da entidade Users

--- src/database/migrations/20220714220751-users.js
+++ src/database/migrations/20220714220751-users.js
@@ -2,20 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    const Users = queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    })
+
+    return Users;
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, __Sequelize) => {
+    await queryInterface.dropTable('Users')
   }
 };
