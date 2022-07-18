b7655b38ea8f30c485eafec3f2b23b4561bd26d6
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-18 20:55:28 -0300

    chore: Req 03 (Controllers) - Remove validaçoes do body

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,38 +1,16 @@
 const jwt = require('jsonwebtoken');
-const loginService = require('../services/loginService');
+// const loginService = require('../services/loginService');
 
 const { JWT_SECRET } = process.env;
 
-const validadeBody = async ({ email, password }, next) => {
-  if (email.length === 0 || password.length === 0) {
-    const err = new Error('Some required fields are missing');
-    err.statusCode = 400;
-
-    return next(err);
-  }
-
-  return { email, password };
-};
-
-const validateEmail = async (email, next) => {
-  const result = await loginService.login(email);
-
-  if (!result) {
-    const err = new Error('Invalid fields');
-    err.statusCode = 400;
-  
-    return next(err);
-  }
-  return result;
-};
-
 module.exports = async (req, res, next) => {
   try {
-    const { error, email, password } = req.body;
-  
-    validadeBody({ email, password }, next);
-    validateEmail(email, next);
-    
+    const { error,
+      //  email, password 
+      } = req.body;
+
+    // const result = await loginService.login(email);
+
     if (error) return next(error);
 
     const payload = {
@@ -44,10 +22,10 @@ module.exports = async (req, res, next) => {
       algorithm: 'HS256',
     });
 
-    res.status(200).json({ token });
+    return res.status(200).json({ token });
   } catch (error) {
     console.error(error.message);
   
-    res.status(500).json({ message: 'Algo deu errado' });
+    return res.status(500).json({ message: 'Algo deu errado' });
   }
 };
