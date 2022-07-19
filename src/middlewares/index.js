1664115e3afde20a2ce93f23f536ed019660b839
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-18 22:51:23 -0300

    chore: Req 05 (Middlewares) - adiciona exportacao do middleware que valida o usuario

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const error = require('./error');
 const auth = require('./auth');
 const validadeBody = require('./validateBody');
+const validadeUser = require('./validateUser');
 
 module.exports = {
   auth,
   error,
   validadeBody,
+  validadeUser,
 };
