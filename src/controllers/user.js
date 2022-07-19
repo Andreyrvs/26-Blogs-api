efe9e5dfa44f84d4293b2064a96beb4171e48056
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-19 17:05:38 -0300

    chore: Req 07 (Controllers) - Adiciona controller para listar o usuario pelo id' g

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,9 @@
 const userService = require('../services/userService');
 
-module.exports = async (_req, res, _next) => {
+const getAll = async (req, res, __next) => {
   try {
     const getUser = await userService.getAll();
-    console.log(getUser);
+
     if (!getUser) {
       return res.status(404).json({ message: 'User Not Found' });
     }
@@ -14,4 +14,26 @@ module.exports = async (_req, res, _next) => {
 
     return res.status(500).json({ message: 'Algo deu errado' });
   }
+};
+
+const getById = async (req, res, __next) => {
+  try {
+    const { id } = req.params;
+    const getUser = await userService.getById(id);
+
+    if (!getUser) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    return res.status(200).json(getUser);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
 };
