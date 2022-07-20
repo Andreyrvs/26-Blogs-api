2bcdf0fcfadc84a282bcbad07362793cf65fd705
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-20 17:42:30 -0300

    chore: Req 12 (Controllers) - Adiciona exportaçao do controller blogPost

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
 const category = require('./category');
 const login = require('./login');
 const user = require('./user');
+const blogPost = require('./blogPost');
 
 module.exports = {
+  blogPost,
   category,
   login,
   user,
