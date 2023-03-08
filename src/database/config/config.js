8e13576e33f3da3bca7167c884cbf2129d5a5a73
Andrey Visniewski <cortaesai@gmail.com>
2023-03-08 17:06:22 -0300

    deploy: (Railway) Sequelize config - Altera o nome do database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -33,19 +33,10 @@
 
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
   port: process.env.MYSQLPORT || '3306',
-  database: 
-    `${process.env.MYSQL_DATABASE || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  database: process.env.MYSQL_DATABASE,
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || '1234',
   dialect: 'mysql',
