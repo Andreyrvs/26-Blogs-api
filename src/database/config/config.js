a0728c6d481e3b43fdcfd027a3c3e1e5115ebe38
Andrey Visniewski <cortaesai@gmail.com>
2023-03-07 21:45:29 -0300

    Ultimo

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,3 +1,36 @@
+// require('dotenv').config();
+
+// const environment = process.env.NODE_ENV || 'test';
+
+// const suffix = {
+//   dev: '-dev',
+//   development: '-dev',
+//   test: '-test',
+// };
+
+// const options = {
+//   host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
+//   port: process.env.MYSQLPORT || '3306',
+//   database: 
+//     `${process.env.MYSQL_DATABASE || 'blogs-api'}${suffix[environment] || suffix.test}`,
+//   username: process.env.MYSQLUSER || 'root',
+//   password: process.env.MYSQLPASSWORD || '1234',
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     timezone: 'Z',
+//   },
+//   logging: process.env.DEBUG !== 'false',
+// };
+
+// module.exports = {
+//   development: {
+//     ...options,
+//   },
+//   test: {
+//     ...options,
+//   },
+// };
+
 require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'test';
